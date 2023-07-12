95da082beae18ae486ac3b284abb9095712a6e22
Ihor_S <ihor.shcherbyna@clockwise.software>
2023-07-12 18:27:29 +0200

    feat: add update file mutation

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -108,6 +108,85 @@ export class FileService {
     }
   }
 
+  // TODO: Decompose this and uploadFile() and pull out reusable parts
+  async updateFile(
+    readStream: ReadStream,
+    id: number,
+    fileName: string,
+    fileType: string,
+    fileSize: number,
+  ): Promise<File> {
+    try {
+      const oldFileEntity = await this.fileRepository.findOneBy({ id });
+      if (!oldFileEntity) throw new Error(`Not found file with id=${id}`);
+
+      const accessKeyId = process.env.AWS_S3_ACCESS_ID;
+      const secretAccessKey = process.env.AWS_S3_SECRET_KEY;
+      const bucketName = process.env.AWS_S3_BUCKET_NAME;
+      const region = process.env.AWS_S3_REGION;
+
+      const newFileKey = `${nanoid()}-${fileName}`;
+
+      const hash = createHash('sha256');
+      let hashValue: string | null = null;
+
+      const calcHashTr = new Transform({
+        transform(chunk, _encoding, callback) {
+          hash.update(chunk);
+          this.push(chunk);
+          callback();
+        },
+        flush(callback) {
+          hashValue = hash.digest('hex') as string;
+          callback();
+        },
+      });
+
+      readStream.pipe(calcHashTr);
+
+      const s3Client = new S3Client({
+        region,
+        credentials: {
+          accessKeyId,
+          secretAccessKey,
+        },
+      });
+
+      const uploadParams = {
+        Bucket: bucketName,
+        Key: newFileKey,
+        Body: calcHashTr,
+      };
+
+      const parallelUploads3 = new Upload({
+        client: s3Client,
+        params: uploadParams,
+        queueSize: 40,
+        partSize: 1024 * 1024 * 5,
+        leavePartsOnError: false,
+      });
+
+      await parallelUploads3.done();
+
+      const deleteParams = {
+        Bucket: bucketName,
+        Key: oldFileEntity.fileUrl.split('/').at(-1),
+      };
+      const deleteCommand = new DeleteObjectCommand(deleteParams);
+      await s3Client.send(deleteCommand);
+      const updatedFileEntity = Object.assign(oldFileEntity, {
+        fileName,
+        fileType,
+        fileSize,
+        fileUrl: `https://${bucketName}.s3.${region}.amazonaws.com/${newFileKey}`,
+        fileHash: hashValue,
+      });
+      return await this.fileRepository.save(updatedFileEntity);
+    } catch (err) {
+      console.log('File update failed', err);
+    }
+  }
+
   async getAll() {
     return await this.fileRepository.find();
   }
